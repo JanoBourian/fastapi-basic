8bf2bee156689855ac1a8ebca89eb1097ac22f68
JanoBourian <supermath33@icloud.com>
2022-06-08 20:20:35 -0500

    Add: I added routes, parameters, documentation, aditional info and typing

--- README.md
+++ README.md
@@ -4,6 +4,16 @@ A basic course of FastAPI topics and knowledges
 ## Start FastAPI
     - uvicorn main:app --reload
 
+## Path and Query Parameters
+    - Dynamic value "/propery/{id}"
+    - Documentation with "/docs" or "/redoc" and remember that you can use: 
+        - tags
+        - summary
+        - description
+        - response_description
+        - status.HTTP_200_OK
+    - Ordering Routes or paths: The order is so much important
+
 ## Generate requirements.txt
     - pip freeze >> requirements.txt
 
--- main.py
+++ main.py
@@ -1,15 +1,60 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
+from typing import Optional
 
 app = FastAPI()
 
-@app.get("/")
-def index():
+@app.get("/", 
+         tags = ["index"],
+         summary = "The index route", 
+         description = "Return a little regards",
+         response_description = "Message")
+def index(response:Response):
+    response.status_code = status.HTTP_200_OK
     return {"message": "in a bottle"}
 
-@app.get("/property")
-def property():
-    return "<h1> This is a property field </h1>"
+@app.get("/property/{id}",
+         tags = ["property", "id"],
+         summary = "Retrieve property by id", 
+         description = "Take id value and return its properties",
+         response_description = "Message with the property id")
+def property(id: int, response:Response):
+    response.status_code = status.HTTP_200_OK
+    return {"message": f"Property by id number {id}"}
 
-@app.get("/movies")
-def movies():
+@app.get('/profile/{username}',
+         tags = ["profile", "username"],
+         summary = "Retrieve profile by username",
+         response_description = "Message with the profile")
+def profile(username: str, response:Response):
+    """ 
+    Simulates the profiles
+    """
+    response.status_code = status.HTTP_200_OK
+    return {"message": f"This is a profile page for user {username}"}
+
+@app.get("/movies",
+         tags = ["movies"], 
+         summary = "Check all movies availables",
+         description = "Return a list of movies availables",
+         response_description = "JSON dict")
+def movies(response:Response):
+    response.status_code = status.HTTP_200_OK
     return {'movie_list': {'Movie 1', 'Movie 2'}} #That will be a list of items
+
+@app.get("/user/admin", 
+         tags = ["user", "admin"],
+         summary = "Return admin info", 
+         description = "This endpoint retrieve the admin contact info", 
+         response_description = "A JSON dict with contact info of our admin")
+def admin(response:Response):
+    response.status_code = status.HTTP_200_OK
+    return {"message": "Admin page"}
+
+@app.get("/user/{username}", 
+         tags = ["user", "username"],
+         summary = "Return username info", 
+         description = "This endpoint retrieve the username contact info", 
+         response_description = "A JSON dict with contact info of a specific username")
+def admin(username:str, response:Response):
+    response.status_code = status.HTTP_200_OK
+    return {"message": f"username {username}"}
